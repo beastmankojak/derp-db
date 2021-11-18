4030b6d460bfdc98138ebc6af38e07e3fc99ce08
Beastman Kojak <beastman.kojak@gmail.com>
2021-11-18 17:24:47 -0500

    Add twins to attributes

--- src/queries/updateDerplingAttributesMaterializedView.js
+++ src/queries/updateDerplingAttributesMaterializedView.js
@@ -44,6 +44,28 @@ const updateDerplingAttributesMaterializedView = async (collection) => {
   for (trait of traits) {
     await collection.aggregate(buildPipeline(trait)).next();
   }
+
+  const twins = await collection.aggregate([
+    { $group: { 
+      _id: 'twins',
+      totalCount: { $sum: 1 },
+      twins: { $sum: { $cond: { if: { $ne: [{ $ifNull: [ '$twin', '' ] }, ''] }, then: 1, else: 0 } } }
+    } },
+    { $addFields: { 
+        notTwins: { $subtract: ['$totalCount', '$twins'] }, 
+        twinsPct: { $multiply: [{$divide: ['$twins', '$totalCount']}, 100] }
+    } },
+    { $addFields: { 
+        notTwinsPct: { $multiply: [{$divide: ['$notTwins', '$totalCount']}, 100] }
+    } },
+    { $project: { 
+        _id: 1,
+        values: { $concatArrays: [ [
+            { yes: { count: '$twins', pct: '$twinsPct' } },    
+            { no: { count: '$notTwins', pct: '$notTwinsPct' } },
+    ] ]} } },
+    { $merge: { into: 'derplingAttributes', on: '_id', whenMatched: 'replace' } }  
+  ]).next();
 };
 
 module.exports = updateDerplingAttributesMaterializedView;
