c61d5188bdf8f825a2293f65bf8fb39bd225d590
Beastman Kojak <beastman.kojak@gmail.com>
2022-02-22 00:27:45 -0500

    Update horse transform to account for missing markings

--- src/projects/equine/custom/horseTransform.js
+++ src/projects/equine/custom/horseTransform.js
@@ -1,9 +1,9 @@
 const _ = require('lodash');
 
 const transform = () => 
-  (asset) => asset.onchain_metadata && asset.onchain_metadata.Markings ? {
+  (asset) => asset.onchain_metadata ? {
     ..._.pick(asset.onchain_metadata, ['name', 'image', 'Color', 'Gender', 'Age', 'speed', 'agility', 'stamina', 'endurance', 'acceleration']),
-    ..._.pick(asset.onchain_metadata.Markings, ['Body', 'Head', 'Back Left Leg', 'Back Right Leg', 'Front Left Leg', 'Front Right Leg']),
+    ..._.pick(asset.onchain_metadata.Markings || {}, ['Body', 'Head', 'Back Left Leg', 'Back Right Leg', 'Front Left Leg', 'Front Right Leg']),
     total: asset.onchain_metadata.speed 
       + asset.onchain_metadata.agility 
       + asset.onchain_metadata.stamina 
