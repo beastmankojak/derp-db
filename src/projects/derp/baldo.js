d8233baeeee5cf49ffe2452023fd6a07a5584add
Beastman Kojak <beastman.kojak@gmail.com>
2022-10-01 22:56:28 -0400

    Add baldo

---
+++ src/projects/derp/baldo.js
@@ -0,0 +1,47 @@
+const {
+  collectionNames: {
+    metaCollection,
+    traitCollection,
+    statsCollection
+  },
+  defaultTraitTransform,
+  defaultRarityTransform,
+  defaultStatsTransform,
+} = require('../../transforms');
+const metaTransform = require('./custom/metaTransform');
+
+const traits = [
+'Beak', 'Body', 'Eyes', 'Head', 'Mouth', 'Background',
+];
+
+const collectionName = 'baldo';
+const metaCollectionName = metaCollection(collectionName);
+const traitCollectionName = traitCollection(collectionName);
+const statsCollectionName = statsCollection(collectionName);
+
+const originTraitMapper = (traits) => traits.map((trait) => `attributes.${trait}`);
+
+const meta = {
+policyId: '60b741294ee35c3de6f06a7060ff27422956f54d50339a4f445423e0',
+db: {
+  name: 'derp',
+  collectionName,
+  metaCollectionName,
+  traitCollectionName,
+  statsCollectionName
+},
+traits,
+};
+
+
+const finalMeta = {
+...meta,
+metaTransform: metaTransform(traits),
+viewTransforms: [
+  defaultTraitTransform(meta),
+  defaultRarityTransform({ ...meta, originTraitMapper }),
+  defaultStatsTransform(meta),
+]
+}
+
+module.exports = finalMeta;
